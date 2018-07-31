4d8997c6c659465b4efcfb7971b1d041a2a2f9cf
altostratous <altostratous@gmail.com>
2018-07-31 06:47:49 +0200

    Add some data structures

---
+++ CodeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VAuth
+{
+    class CodeBook
+    {
+        private List<VoiceIdentity> identities;
+
+        public CodeBook(List<VoiceIdentity> identities)
+        {
+            this.identities = identities;
+        }
+    }
+}
---
+++ VoiceIdentity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace VAuth
+{
+    class VoiceIdentity
+    {
+        public string tag;
+        private string password;
+        private List<string> mfccFiles;
+
+        public VoiceIdentity(string tag, string password, List<string> mfccFiles)
+        {
+            this.tag = tag;
+            this.password = password;
+            this.mfccFiles = mfccFiles;
+        }
+    }
+}
